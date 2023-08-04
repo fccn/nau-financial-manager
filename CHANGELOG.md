6299585af0d0bed1206e4c246490339859809f4b
jamo.qs <jamo.qs@gmail.com>
2023-08-04 17:40:20 +0100

    refact: innitial setup of project

---
+++ CHANGELOG.md
@@ -0,0 +1,5 @@
+
+[unreleased]: https://github.com/iworks-pt/iWorks-MS0-Users
+[0.0.1] - undefined date
+
+- Create innitial setup for the project
--- nau_receipts_manager/settings.py
+++ nau_receipts_manager/settings.py
@@ -9,26 +9,17 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+from decouple import Csv, config
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = config("SECRET_KEY")
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+DEBUG = config("DEBUG", default=False, cast=bool)
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-89x(2gbrxdo8z-7g3no54j2eagxr=awkrrr*g&hv*#uwh5^ui1"
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = ["localhost"]
-
-
-# Application definition
+ALLOWED_HOSTS = config("ALLOWED_HOSTS", cast=Csv, default="localhost")
 
 INSTALLED_APPS = [
     "django.contrib.admin",
@@ -71,20 +62,12 @@ TEMPLATES = [
 
 WSGI_APPLICATION = "nau_receipts_manager.wsgi.application"
 
-
-# Database
-# https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": config("DATABASE_NAME", default="db.sqlite3"),
     }
 }
-
-
-# Password validation
-# https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -101,10 +84,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/4.2/topics/i18n/
-
 LANGUAGE_CODE = "en-us"
 
 TIME_ZONE = "UTC"
@@ -113,13 +92,6 @@ USE_I18N = True
 
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
-
 STATIC_URL = "static/"
-
-# Default primary key field type
-# https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
