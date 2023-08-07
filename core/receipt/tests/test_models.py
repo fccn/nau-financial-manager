8881e90f4593fe6b9bf090a3740cb236b532f1be
CaikeC <cantonio@quanticosolutions.com>
2023-08-07 15:09:26 +0100

    feat: Tests for receipt and receiptline

--- core/receipt/tests/test_models.py
+++ core/receipt/tests/test_models.py
@@ -12,6 +12,26 @@ class ReceiptModelTestCase(TestCase):
         receipt = ReceiptFactory(name="John Doe")
         self.assertEqual(str(receipt), "John Doe")
 
+    def test_receipt_update(self):
+        receipt = ReceiptFactory(name="Initial Name")
+        new_name = "Updated Name"
+        receipt.name = new_name
+        receipt.save()
+        updated_receipt = Receipt.objects.get(pk=receipt.pk)
+        self.assertEqual(updated_receipt.name, new_name)
+
+    def test_receipt_delete(self):
+        receipt = ReceiptFactory()
+        receipt_pk = receipt.pk
+        receipt.delete()
+        with self.assertRaises(Receipt.DoesNotExist):
+            Receipt.objects.get(pk=receipt_pk)
+
+    def test_receipt_read(self):
+        receipt = ReceiptFactory()
+        retrieved_receipt = Receipt.objects.get(pk=receipt.pk)
+        self.assertEqual(receipt, retrieved_receipt)
+
 
 class ReceiptLineModelTestCase(TestCase):
     def test_receipt_line_creation(self):
@@ -21,3 +41,23 @@ class ReceiptLineModelTestCase(TestCase):
     def test_receipt_line_str_representation(self):
         receipt_line = ReceiptLineFactory(description="Product A")
         self.assertEqual(str(receipt_line), "Product A")
+
+    def test_receipt_line_update(self):
+        receipt_line = ReceiptLineFactory(description="Initial Description")
+        new_description = "Updated Description"
+        receipt_line.description = new_description
+        receipt_line.save()
+        updated_receipt_line = ReceiptLine.objects.get(pk=receipt_line.pk)
+        self.assertEqual(updated_receipt_line.description, new_description)
+
+    def test_receipt_line_delete(self):
+        receipt_line = ReceiptLineFactory()
+        receipt_line_pk = receipt_line.pk
+        receipt_line.delete()
+        with self.assertRaises(ReceiptLine.DoesNotExist):
+            ReceiptLine.objects.get(pk=receipt_line_pk)
+
+    def test_receipt_line_read(self):
+        receipt_line = ReceiptLineFactory()
+        retrieved_receipt_line = ReceiptLine.objects.get(pk=receipt_line.pk)
+        self.assertEqual(receipt_line, retrieved_receipt_line)
