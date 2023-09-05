0e46272add2baa9308c999583c841fd151928d17
jamo.qs <jamo.qs@gmail.com>
2023-09-05 09:24:46 +0100

    chore: add admin area for modules organization and shared_revenue

--- core/shared_revenue/admin.py
+++ core/shared_revenue/admin.py
@@ -1,3 +1,26 @@
-# from django.contrib import admin
+from django.contrib import admin
 
-# Register your models here.
+from core.shared_revenue.models import PartnershipLevel, RevenueConfiguration, ShareExecution
+
+
+class PartnershipLevelAdmin(admin.ModelAdmin):
+    list_display = ("name", "description", "percentage", "created_at", "updated_at")
+    search_fields = ("name", "description", "percentage")
+    list_filter = ("created_at", "updated_at")
+
+
+class RevenueConfigurationAdmin(admin.ModelAdmin):
+    list_display = ("organization", "course_code", "partnership_level", "start_date", "end_date")
+    search_fields = ("organization", "course_code", "partnership_level", "start_date", "end_date")
+    list_filter = ("created_at", "updated_at")
+
+
+class ShareExecutionAdmin(admin.ModelAdmin):
+    list_display = ("organization", "course_code", "partnership_level", "start_date", "end_date")
+    search_fields = ("organization", "course_code", "partnership_level", "start_date", "end_date")
+    list_filter = ("created_at", "updated_at")
+
+
+admin.site.register(PartnershipLevel)
+admin.site.register(RevenueConfiguration)
+admin.site.register(ShareExecution)
