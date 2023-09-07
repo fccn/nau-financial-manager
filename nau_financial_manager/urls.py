5e774615b591d19e59fba03d3eba7c403ebf0e2d
jamo.qs <jamo.qs@gmail.com>
2023-09-07 10:50:42 +0100

    chore: add customized swagger infos abstracted with dotenv

--- nau_financial_manager/urls.py
+++ nau_financial_manager/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api-auth/", include("rest_framework.urls")),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("api/docs/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
-    path("api/redocs/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    path("api/docs/", SpectacularSwaggerView.as_view(), name="swagger-ui"),
+    path("api/redocs/", SpectacularRedocView.as_view(), name="redoc"),
 ]
