7ca935154aad4a35eeb8311f24590d3431baadbb
jamo.qs <jamo.qs@gmail.com>
2023-09-15 14:29:02 +0100

    feat: adds abstract user to add more attributes in the future

--- apps/util/models.py
+++ apps/util/models.py
@@ -1,8 +1,15 @@
+from uuid import uuid4
+
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 from safedelete.models import SOFT_DELETE_CASCADE, SafeDeleteModel
 
 
 class BaseModel(SafeDeleteModel):
+    """
+    Abstract base model that provides common fields for all models in the project.
+    """
+
     _safedelete_policy = SOFT_DELETE_CASCADE
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
@@ -10,3 +17,15 @@ class BaseModel(SafeDeleteModel):
 
     class Meta:
         abstract = True
+
+
+class CustomUser(AbstractUser):
+    """
+    Custom user model that extends the built-in Django user model.
+    """
+
+    uuid = models.UUIDField("uuid", default=uuid4, editable=False)
+    birth_date = models.DateField(null=True, blank=True)
+
+    def __str__(self):
+        return self.email
