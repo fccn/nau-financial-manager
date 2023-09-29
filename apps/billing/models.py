19b871d83c1de955ec9bc334b54d581f2fa4fae2
Tiago-da-Silva23 <tsilva@quanticosolutions.com>
2023-09-29 15:08:59 +0100

    Refactored the models to the expected business role

--- apps/billing/models.py
+++ apps/billing/models.py
@@ -3,7 +3,21 @@ from uuid import uuid4
 from django.db import models
 from django_countries.fields import CountryField
 
+from apps.organization.models import Organization
 from apps.util.models import BaseModel
+
+
+class Product(BaseModel):
+
+    """
+    This model that represents the bought product
+    Each ReceiptItem is a product, item price is the product price
+    """
+
+    organization = models.ForeignKey(
+        Organization, on_delete=models.CASCADE, related_name="product_organization", null=True, blank=True
+    )
+    product_id = models.CharField(max_length=100, null=True, blank=True)
 
 
 class Receipt(BaseModel):
@@ -81,11 +95,7 @@ class ReceiptItem(BaseModel):
     amount_exclude_vat = models.DecimalField(max_digits=10, decimal_places=2, null=True)
     amount_include_vat = models.DecimalField(max_digits=10, decimal_places=2, null=True)
     organization_code = models.CharField(max_length=255, null=True)
-    course_code = models.CharField(max_length=255, null=True)
-    course_id = models.CharField(max_length=255, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="item_product", null=True, blank=True)
 
     def __str__(self):
         return self.description
-
-    class Meta:
-        constraints = [models.UniqueConstraint(fields=["receipt"], name="unique_receipt_item")]
--- apps/shared_revenue/models.py
+++ apps/shared_revenue/models.py
@@ -3,7 +3,7 @@ from django.db import models
 from django.db.models import CheckConstraint, Q
 from django.utils.translation import gettext_lazy as _
 
-from apps.billing.models import Receipt
+from apps.billing.models import Product, Receipt
 from apps.organization.models import Organization
 from apps.util.models import BaseModel
 
@@ -11,12 +11,18 @@ from apps.util.models import BaseModel
 class PartnershipLevel(BaseModel):
     """
     A model representing a partnership level.
+
+    The field percentage is the product price amount divided for each organization, the default percentage
+    is 70% and if there are more than one organzation for this product, the value of 70% will be divided
+    as the business partnership contract works
+
     """
 
     name = models.CharField(_("Name"), max_length=50, unique=True)
     description = models.CharField(_("Description"), max_length=255, null=True, blank=True)
     percentage = models.DecimalField(
         _("Value"),
+        default=0.70,
         max_digits=3,
         validators=[MaxValueValidator(1), MinValueValidator(0)],
         decimal_places=2,
@@ -36,15 +42,15 @@ class RevenueConfiguration(BaseModel):
     A model representing a revenue configuration for an organization and partnership level.
     """
 
+    start_date = models.DateTimeField(_("Start date"), auto_now_add=True)
+    end_date = models.DateTimeField(_("End date"), null=True, blank=True)
     organization = models.ForeignKey(
         Organization, on_delete=models.CASCADE, related_name="revenue_organizations", null=True, blank=True
     )
-    course_code = models.CharField(_("Course code"), max_length=50, null=True, blank=True)
     partnership_level = models.ForeignKey(
         PartnershipLevel, on_delete=models.CASCADE, related_name="revenue_partnership_levels"
     )
-    start_date = models.DateTimeField(_("Start date"), auto_now_add=True)
-    end_date = models.DateTimeField(_("End date"), null=True, blank=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name="item_product", null=True, blank=True)
 
     class Meta:
         verbose_name = _("Revenue configuration")
