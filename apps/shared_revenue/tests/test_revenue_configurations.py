e8f1c67d687ff7e653578da3eed05aa65c7cf852
Tiago-da-Silva23 <tsilva@quanticosolutions.com>
2023-10-27 17:44:12 +0100

    feat: created tests for each possible situation of the RevenueConfiguration model

--- apps/shared_revenue/tests/test_revenue_configurations.py
+++ apps/shared_revenue/tests/test_revenue_configurations.py
@@ -1,9 +1,14 @@
+from datetime import datetime, timedelta
+
+import factory
 from django.core.exceptions import ValidationError
-from django.db.utils import IntegrityError
+from django.db import IntegrityError
 from django.test import TestCase
+from django.utils import timezone
 
 from apps.organization.factories import OrganizationFactory
 from apps.shared_revenue.factories import RevenueConfigurationFactory
+from apps.shared_revenue.models import RevenueConfiguration
 
 
 class RevenueConfigurationTestCase(TestCase):
@@ -13,130 +18,249 @@ class RevenueConfigurationTestCase(TestCase):
 
     def test_str_method(self):
         """
-        Test the `__str__` method of the `RevenueConfiguration` model.
+        Should validate that the `RevenueConfiguration.__str__` method returns the correct parameters in the correct sequence.
         """
+
         self.assertEqual(
-            str(self.revenue_configuration), f"{self.organization} - {self.revenue_configuration.product_code}"
+            str(self.revenue_configuration),
+            f"{self.organization} - {self.revenue_configuration.product_id} - {self.revenue_configuration.partner_percentage}",
         )
 
-    def test_organization_or_product_code_without_course_or_organization(self):
+    def test_valid_partner_percentage(self):
         """
-        Test that attempting to create a `RevenueConfiguration` instance without an `organization`
-        or `product_code` raises an `IntegrityError`.
+        Should validate that the `partner_percentage` is between 0 and 1.
+        """
+
+        self.assertGreaterEqual(self.revenue_configuration.partner_percentage, 0)
+        self.assertLessEqual(self.revenue_configuration.partner_percentage, 1)
+
+    def test_required_parameters(self):
+        """
+        Should raise an error of type `IntegrityError`, it validates the required parameters to save a `RevenueConfiguration`.
         """
         with self.assertRaises(IntegrityError):
-            RevenueConfigurationFactory(organization=None, product_code=None)
+            RevenueConfigurationFactory(
+                partner_percentage=None,
+                product_id=None,
+            )
 
-    def test_organization_and_product_code_can_not_be_both_filled(self):
+    def test_has_concurrent_revenue_configuration_with_dates(self):
         """
-        Test that attempting to create a `RevenueConfiguration` instance with both an `organization`
-        and a `product_code` raises an `IntegrityError`.
+        Should raise an error of type `ValidationError`, it validates that
+        already exists a `RevenueConfiguration` for this `Organization` and `product_id`
+        for the concurrent period of time.
         """
-        with self.assertRaises(IntegrityError):
-            RevenueConfigurationFactory(organization=self.organization, product_code="ABC123")
 
-    def test_organization_null(self):
-        """
-        Test that attempting to create a `RevenueConfiguration` instance with a null `organization`
-        and null `product_code` raises a `ValidationError`.
-        """
-        with self.assertRaises(ValidationError):
-            RevenueConfigurationFactory(product_code="ABC123")
+        with self.assertRaisesMessage(
+            expected_exception=ValidationError,
+            expected_message="There is a concurrent revenue configuration in this moment",
+        ):
+            RevenueConfigurationFactory(
+                start_date=factory.Faker(
+                    "date_time_between", start_date="+1d", end_date="+20d", tzinfo=timezone.get_current_timezone()
+                ),
+                end_date=factory.Faker(
+                    "date_time_between", start_date="+30d", end_date="+69d", tzinfo=timezone.get_current_timezone()
+                ),
+                product_id=self.revenue_configuration.product_id,
+                organization=self.organization,
+            )
 
-    def test_product_code_null(self):
+    def test_has_concurrent_revenue_configuration_with_just_start_date(self):
         """
-        Test that attempting to create a `RevenueConfiguration` instance with a null `product_code`
-        and null `organization` raises a `ValidationError`.
+        Should raise an error of type `ValidationError`, it validates that
+        already exists a `RevenueConfiguration` for this `Organization` and `product_id`
+        for the concurrent period of time, considering that the attempt is to save a None `end_date`.
         """
-        with self.assertRaises(ValidationError):
-            RevenueConfigurationFactory(organization=self.organization)
 
-    def test_start_date_is_now(self):
-        """
-        Test that the `start_date` field is automatically set to the current date and time when a
-        `RevenueConfiguration` instance is created.
-        """
-        revenue_configuration = RevenueConfigurationFactory(organization=self.organization)
-        self.assertIsNotNone(revenue_configuration.start_date)
+        with self.assertRaisesMessage(
+            expected_exception=ValidationError,
+            expected_message="There is a concurrent revenue configuration in this moment",
+        ):
+            RevenueConfigurationFactory(
+                start_date=factory.Faker(
+                    "date_time_between", start_date="+1d", end_date="+20d", tzinfo=timezone.get_current_timezone()
+                ),
+                end_date=None,
+                product_id=self.revenue_configuration.product_id,
+                organization=self.organization,
+            )
 
-    def test_end_date_null(self):
+    def test_has_concurrent_revenue_configuration_with_just_end_date(self):
         """
-        Test that the `end_date` field can be null when creating a `RevenueConfiguration` instance.
+        Should raise an error of type `ValidationError`, it validates that
+        already exists a `RevenueConfiguration` for this `Organization` and `product_id`
+        for concurrent period of time, considering that the attempt is to save a None `start_date`.
         """
-        revenue_configuration = RevenueConfigurationFactory(organization=self.organization, end_date=None)
-        self.assertIsNone(revenue_configuration.end_date)
 
-    def test_end_date_not_null(self):
-        """
-        Test that the `end_date` field cannot be null when creating a `RevenueConfiguration` instance.
-        """
-        with self.assertRaises(IntegrityError):
-            RevenueConfigurationFactory(organization=self.organization, end_date=None)
+        with self.assertRaisesMessage(
+            expected_exception=ValidationError,
+            expected_message="There is a concurrent revenue configuration in this moment",
+        ):
+            RevenueConfigurationFactory(
+                start_date=None,
+                end_date=factory.Faker(
+                    "date_time_between", start_date="+30d", end_date="+69d", tzinfo=timezone.get_current_timezone()
+                ),
+                product_id=self.revenue_configuration.product_id,
+                organization=self.organization,
+            )
 
-    def test_product_code_max_length(self):
+    def test_has_concurrent_revenue_configuration_with_no_dates(self):
         """
-        Test that attempting to create a `RevenueConfiguration` instance with a `product_code`
-        that exceeds the maximum length raises a `ValidationError`.
+        Should raise an error of type `ValidationError`, it validates that
+        already exists a `RevenueConfiguration` for this `Organization` and `product_id`
+        in the concurrent period of time, considering that the attempt is to save
+        as None the `start_date` and `end_date` parameters.
         """
-        long_product_code = "a" * 51
-        with self.assertRaises(ValidationError):
-            RevenueConfigurationFactory(organization=self.organization, product_code=long_product_code)
 
-    def test_product_code_blank(self):
-        """
-        Test that the `product_code` field can be blank when creating a `RevenueConfiguration` instance.
-        """
-        revenue_configuration = RevenueConfigurationFactory(organization=self.organization, product_code="")
-        self.assertEqual(revenue_configuration.product_code, "")
+        with self.assertRaisesMessage(
+            expected_exception=ValidationError,
+            expected_message="There is a concurrent revenue configuration in this moment",
+        ):
+            RevenueConfigurationFactory(
+                start_date=None,
+                end_date=None,
+                product_id=self.revenue_configuration.product_id,
+                organization=self.organization,
+            )
 
-    def test_product_code_and_organization_null(self):
+    def test_has_no_concurrent_revenue_configuration(self):
         """
-        Test that attempting to create a `RevenueConfiguration` instance with both `product_code`
-        and `organization` null raises a `ValidationError`.
+        Should register a new `RevenueConfiguration`, it validades that this attempt
+        is to save a new configuration which will not impact on the current, or any other configuration.
         """
-        with self.assertRaises(ValidationError):
-            RevenueConfigurationFactory(organization=None, product_code=None)
 
-    def test_organization_foreign_key(self):
-        """
-        Test that the `organization` field is a foreign key to the `Organization` model.
-        """
-        revenue_configuration = RevenueConfigurationFactory(organization=self.organization)
-        self.assertEqual(revenue_configuration.organization, self.organization)
-
-    def test_organization_related_name(self):
-        """
-        Test that the `related_name` argument for the `organization` field is set to `"revenue_organizations"`.
-        """
-        self.assertIn(self.organization)
-
-    def test_organization_or_product_code_constraint(self):
-        """
-        Test that the `organization_or_product_code` constraint is enforced.
-        """
-        with self.assertRaises(IntegrityError):
-            RevenueConfigurationFactory(organization=None, product_code=None)
-
-    def test_end_date_equal_to_start_date(self):
-        """
-        Test that the `end_date` field can be equal to the `start_date` field when
-        creating a `RevenueConfiguration` instance.
-        """
-        revenue_configuration = RevenueConfigurationFactory(
+        new_configuration = RevenueConfigurationFactory(
+            start_date=factory.Faker(
+                "date_time_between", start_date="+71d", end_date="+80d", tzinfo=timezone.get_current_timezone()
+            ),
+            end_date=factory.Faker(
+                "date_time_between", start_date="+81d", end_date="+90d", tzinfo=timezone.get_current_timezone()
+            ),
             organization=self.organization,
-            start_date="2022-01-01 00:00:00",
-            end_date="2022-01-01 00:00:00",
+            product_id=self.revenue_configuration.product_id,
         )
-        self.assertEqual(revenue_configuration.start_date, revenue_configuration.end_date)
 
-    def test_end_date_greater_than_start_date(self):
+        self.assertNotEqual(new_configuration.id, self.revenue_configuration.id)
+        self.assertEqual(new_configuration.organization, self.revenue_configuration.organization)
+        self.assertEqual(new_configuration.product_id, self.revenue_configuration.product_id)
+        self.assertTrue(new_configuration.start_date != self.revenue_configuration.start_date)
+        self.assertTrue(new_configuration.end_date != self.revenue_configuration.end_date)
+
+    def test_future_configuration_creation(self):
         """
-        Test that the `end_date` field can be greater than the `start_date` field when
-        creating a `RevenueConfiguration` instance.
+        Should register a new `RevenueConfiguration`, it validades that this attempt
+        is to save a future configuration considering as None the `end_date`, which will not impact
+        on the current, or any other configuration.
         """
-        revenue_configuration = RevenueConfigurationFactory(
+
+        new_configuration = RevenueConfigurationFactory(
+            start_date=factory.Faker(
+                "date_time_between", start_date="+82d", end_date="+90d", tzinfo=timezone.get_current_timezone()
+            ),
+            end_date=None,
             organization=self.organization,
-            start_date="2021-01-01 00:00:00",
-            end_date="2022-01-01 00:00:00",
+            product_id=self.revenue_configuration.product_id,
         )
-        self.assertLess(revenue_configuration.start_date, revenue_configuration.end_date)
+        self.assertNotEqual(new_configuration.id, self.revenue_configuration.id)
+        self.assertEqual(new_configuration.organization, self.revenue_configuration.organization)
+        self.assertEqual(new_configuration.product_id, self.revenue_configuration.product_id)
+        self.assertTrue(new_configuration.start_date > self.revenue_configuration.start_date)
+        self.assertIsNone(new_configuration.end_date)
+
+    def test_older_configuration_creation(self):
+        """
+        Should register a new `RevenueConfiguration`, it validades that this attempt
+        is to save a older configuration, which will not impact on the current, or any other configuration.
+        """
+
+        new_configuration = RevenueConfigurationFactory(
+            start_date=factory.Faker(
+                "date_time_between", start_date="-10d", end_date="-5d", tzinfo=timezone.get_current_timezone()
+            ),
+            end_date=factory.Faker(
+                "date_time_between", start_date="-4d", end_date="-1d", tzinfo=timezone.get_current_timezone()
+            ),
+            organization=self.organization,
+            product_id=self.revenue_configuration.product_id,
+        )
+        self.assertNotEqual(new_configuration.id, self.revenue_configuration.id)
+        self.assertEqual(new_configuration.organization, self.revenue_configuration.organization)
+        self.assertEqual(new_configuration.product_id, self.revenue_configuration.product_id)
+        self.assertTrue(new_configuration.start_date < self.revenue_configuration.start_date)
+        self.assertTrue(new_configuration.end_date < self.revenue_configuration.start_date)
+
+    def test_edit_configuration(self):
+        """
+        Should edit a `RevenueConfiguration`, it validades that this attempt
+        is to edit an existing configuration in a way that will not impact on the current,
+        or any other configuration.
+        """
+
+        new_start_date = datetime.now(tz=timezone.get_current_timezone()) - timedelta(days=3)
+        new_end_date = datetime.now(tz=timezone.get_current_timezone()) - timedelta(days=1)
+
+        self.assertEqual(type(self.revenue_configuration.start_date), datetime)
+        self.assertEqual(type(self.revenue_configuration.end_date), datetime)
+        self.assertNotEqual(self.revenue_configuration.start_date, new_start_date)
+        self.assertNotEqual(self.revenue_configuration.end_date, new_end_date)
+        self.assertNotEqual(self.revenue_configuration.product_id, "new_product_id")
+        self.assertNotEqual(self.revenue_configuration.partner_percentage, 0.71)
+
+        self.revenue_configuration.start_date = new_start_date
+        self.revenue_configuration.end_date = new_end_date
+        self.revenue_configuration.partner_percentage = 0.71
+        self.revenue_configuration.product_id = "new_product_id"
+
+        self.assertEqual(type(self.revenue_configuration.start_date), datetime)
+        self.assertEqual(type(self.revenue_configuration.end_date), datetime)
+        self.assertEqual(self.revenue_configuration.start_date, new_start_date)
+        self.assertEqual(self.revenue_configuration.end_date, new_end_date)
+        self.assertEqual(self.revenue_configuration.partner_percentage, 0.71)
+        self.assertEqual(self.revenue_configuration.product_id, "new_product_id")
+        self.assertEqual(type(self.revenue_configuration), RevenueConfiguration)
+        self.revenue_configuration.save()
+
+    def test_edit_has_concurrent_revenue_configuration(self):
+        """
+        Should raise an error of type `ValidationError`, it validades that is not possible to edit
+        an existing configuration in a way that will impact on the current, or any other configuration.
+        """
+
+        with self.assertRaisesMessage(
+            expected_exception=ValidationError,
+            expected_message="There is a concurrent revenue configuration in this moment",
+        ):
+            new_configuration = RevenueConfigurationFactory(
+                start_date=factory.Faker(
+                    "date_time_between", start_date="-10d", end_date="-5d", tzinfo=timezone.get_current_timezone()
+                ),
+                end_date=factory.Faker(
+                    "date_time_between", start_date="-4d", end_date="-1d", tzinfo=timezone.get_current_timezone()
+                ),
+                organization=self.organization,
+                product_id=self.revenue_configuration.product_id,
+            )
+
+            new_start_date = new_configuration.start_date + timedelta(days=1)
+            new_end_date = new_configuration.end_date - timedelta(days=1)
+
+            self.assertEqual(type(new_start_date), datetime)
+            self.assertEqual(type(new_end_date), datetime)
+            self.assertEqual(type(new_configuration.start_date), datetime)
+            self.assertEqual(type(new_configuration.end_date), datetime)
+            self.assertEqual(type(self.revenue_configuration.start_date), datetime)
+            self.assertEqual(type(self.revenue_configuration.end_date), datetime)
+            self.assertEqual(type(new_configuration), RevenueConfiguration)
+
+            self.assertNotEqual(self.revenue_configuration.start_date, new_start_date)
+            self.assertNotEqual(self.revenue_configuration.end_date, new_end_date)
+            self.assertNotEqual(new_configuration.start_date, self.revenue_configuration.start_date)
+            self.assertNotEqual(new_configuration.end_date, self.revenue_configuration.end_date)
+
+            self.revenue_configuration.start_date = new_start_date
+            self.revenue_configuration.end_date = new_end_date
+
+            self.assertEqual(type(self.revenue_configuration), RevenueConfiguration)
+            self.revenue_configuration.save()
