b210dcc84bb78e4c7d97d67b65a9ae98875ad7a1
jamo.qs <jamo.qs@gmail.com>
2023-11-06 16:40:10 +0000

    refactor: remove unused class models, adjust, views, serializers adn factories

--- apps/organization/admin.py
+++ apps/organization/admin.py
@@ -1,25 +1,11 @@
 from django.contrib import admin
 
-from apps.organization.models import Organization, OrganizationAddress, OrganizationContact
-
-
-class OrganizationAddressInline(admin.TabularInline):
-    model = OrganizationAddress
-    extra = 0
-
-
-class OrganizationContactInline(admin.TabularInline):
-    model = OrganizationContact
-    extra = 0
+from apps.organization.models import Organization
 
 
 class OrganizationAdmin(admin.ModelAdmin):
-    inlines = [
-        OrganizationAddressInline,
-        OrganizationContactInline,
-    ]
-    list_display = ("name", "short_name", "created_at", "updated_at", "is_active")
-    search_fields = ("name", "slug", "short_name")
+    list_display = ("name", "short_name", "email", "created_at", "updated_at", "is_active")
+    search_fields = ("name", "email", "short_name")
 
 
 admin.site.register(Organization, OrganizationAdmin)
