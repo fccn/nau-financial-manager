f833562dc2575a862468aea6ab712b1a58e5190e
jamo.qs <jamo.qs@gmail.com>
2023-11-21 22:08:29 -0300

    tests: setup and create fields check

--- apps/organization/tests/test_organizations.py
+++ apps/organization/tests/test_organizations.py
@@ -1,51 +1,51 @@
+import factory
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from rest_framework.authtoken.models import Token
+from rest_framework.test import APIClient
 
-from apps.organization.factories import OrganizationAddressFactory, OrganizationContactFactory, OrganizationFactory
-from apps.organization.models import OrganizationAddress, OrganizationContact
-
-
-class OrganizationTest(TestCase):
-    def test_organization_str(self):
-        organization = OrganizationFactory()
-        self.assertEqual(str(organization), organization.name)
-
-    def test_organization_uuid_primary_key(self):
-        organization = OrganizationFactory()
-        self.assertIsNotNone(organization.uuid)
-
-    def test_organization_vat_country_default(self):
-        organization = OrganizationFactory()
-        self.assertEqual(organization.vat_country, "PT")
+from apps.organization.factories import OrganizationFactory
+from apps.organization.models import Organization
 
 
-class OrganizationAddressTest(TestCase):
-    def test_organization_address_str(self):
-        organization_address = OrganizationAddressFactory()
-        expected_str = f"{organization_address.organization.name} - {organization_address.address_type}"
-        self.assertEqual(str(organization_address), expected_str)
+class OrganizationModelTest(TestCase):
+    def setUp(self):
+        """
+        Set up the test case by creating a client, endpoint, transaction, transaction item, and payload.
+        Also create a new user and generate a token for that user.
+        """
+        self.client = APIClient()
+        self.endpoint = "/api/organization/organizations"
 
-    def test_organization_address_verbose_name_plural(self):
-        self.assertEqual(str(OrganizationAddress._meta.verbose_name_plural), "Organizations addresses")
+        self.organization = factory.build(dict, FACTORY_CLASS=OrganizationFactory)
+        self.payload = self.organization
+        OrganizationFactory.create()
 
+        # Create a new user and generate a token for that user
+        self.user = get_user_model().objects.create_user(username="testuser", password="testpass")
+        self.token = Token.objects.create(user=self.user)
 
-class OrganizationContactTest(TestCase):
-    def test_organization_contact_str(self):
-        organization_contact = OrganizationContactFactory()
-        expected_str = f"{organization_contact.organization.name} - {organization_contact.contact_type}"
-        self.assertEqual(str(organization_contact), expected_str)
+    def test_name_label(self):
+        organization = Organization.objects.get(id=1)
+        field_label = organization._meta.get_field("name").verbose_name
+        self.assertEquals(field_label, "Name")
 
-    def test_organization_contact_verbose_name_plural(self):
-        self.assertEqual(str(OrganizationContact._meta.verbose_name_plural), "Organizations contacts")
+    def test_short_name_label(self):
+        organization = Organization.objects.get(id=1)
+        field_label = organization._meta.get_field("short_name").verbose_name
+        self.assertEquals(field_label, "Short Name")
 
-    def test_organization_has_only_one_main_contact(self):
-        organization_contact = OrganizationContactFactory(is_main=True)
-        with self.assertRaises(Exception):
-            OrganizationContactFactory(
-                organization=organization_contact.organization,
-                contact_type=organization_contact.contact_type,
-                is_main=True,
-            )
+    def test_email_label(self):
+        organization = Organization.objects.get(id=1)
+        field_label = organization._meta.get_field("email").verbose_name
+        self.assertEquals(field_label, "Email")
 
-    def test_organization_contact_description_blank(self):
-        organization_contact = OrganizationContactFactory(description="")
-        self.assertIsNone(organization_contact.description)
+    def test_name_max_length(self):
+        organization = Organization.objects.get(id=1)
+        max_length = organization._meta.get_field("name").max_length
+        self.assertEquals(max_length, 255)
+
+    def test_object_name_is_name(self):
+        organization = Organization.objects.get(id=1)
+        expected_object_name = f"{organization.name}"
+        self.assertEquals(expected_object_name, str(organization))
