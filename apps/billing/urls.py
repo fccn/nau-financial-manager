b05e0ffeb60b379d9f9e82f03c44f1a5766420a4
Tiago-da-Silva23 <tsilva@quanticosolutions.com>
2023-11-23 18:50:27 +0000

    feat: created first architecture proposal to connect to ilink

--- apps/billing/urls.py
+++ apps/billing/urls.py
@@ -8,4 +8,9 @@ urlpatterns = [
         views.ProcessTransaction.as_view(),
         name="transaction_complete",
     ),
+    path(
+        "invoice-link/<str:transaction_id>/",
+        views.get_invoice_link,
+        name="get_invoice_link",
+    ),
 ]
--- apps/billing/views.py
+++ apps/billing/views.py
@@ -1,7 +1,12 @@
+from django.core.exceptions import ObjectDoesNotExist
 from rest_framework.authentication import TokenAuthentication
+from rest_framework.decorators import api_view, authentication_classes
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 from rest_framework.views import APIView
+from services.document_host_service import InvoiceDocumentHost
 
+from apps.billing.models import Transaction
 from apps.billing.serializers import ProcessTransactionSerializer
 from apps.util.base_views import GeneralPost
 
@@ -10,3 +15,23 @@ class ProcessTransaction(APIView, GeneralPost):
     authentication_classes = [TokenAuthentication]
     serializer = ProcessTransactionSerializer
     permission_classes = [IsAuthenticated]
+
+
+@api_view(["GET"])
+@authentication_classes([TokenAuthentication])
+def get_invoice_link(request, *args, **kwargs):
+    try:
+        if not kwargs:
+            return Response({"response": "Invalid transaction id"}, status=400)
+
+        transaction = None
+        try:
+            transaction = Transaction.objects.get(transaction_id=kwargs["transaction_id"])
+        except ObjectDoesNotExist:
+            return Response({"response": "Trasaction not found"}, status=404)
+
+        invoice_link = InvoiceDocumentHost().get_document(document_id=transaction.document_id)
+
+        return Response({"response": invoice_link}, status=200)
+    except Exception as e:
+        raise e
