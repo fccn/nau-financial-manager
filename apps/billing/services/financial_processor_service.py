5f5604d5ead243ab4a8c5919071d5ac17325b486
Tiago-da-Silva23 <tsilva@quanticosolutions.com>
2023-12-11 17:14:35 +0000

    refactor: refactored the TransactionService and its test

--- apps/billing/services/financial_processor_service.py
+++ apps/billing/services/financial_processor_service.py
@@ -8,25 +8,11 @@ class TransactionProcessorInterface:
     Each new transcation processor needs to implements its logic by signing to this class.
     """
 
-    def send_transaction_to_processor(self, transaction: Transaction) -> dict:
+    transaction: Transaction = None
+
+    def send_transaction_to_processor(self) -> dict:
         raise Exception("This method needs to be implemented")
 
-
-class ProcessorInstantiator:
-    """
-    This class is a instantiator module for the `TransactionProcessorInterface` type.
-
-    The business logic implemented here, deals with invert the dependency for the type `TransactionProcessorInterface`,
-    then a new transaction processor can replace an older without many problems.
-    """
-
-    def __new__(cls, processor: TransactionProcessorInterface) -> TransactionProcessorInterface:
-        if not issubclass(processor, TransactionProcessorInterface):
-            raise Exception("The given processor is not from the type of TransactionProcessorInterface")
-
-        instance = processor()
-
-        if not isinstance(instance, TransactionProcessorInterface):
-            raise Exception("The generated processor instance is not from the type of TransactionProcessorInterface")
-
-        return instance
+    @property
+    def data(self):
+        raise Exception("This method needs to be implemented")
--- apps/billing/services/transaction_service.py
+++ apps/billing/services/transaction_service.py
@@ -1,7 +1,7 @@
 import xmltodict
 
-from apps.billing.models import Transaction
-from apps.billing.services.financial_processor_service import ProcessorInstantiator, TransactionProcessorInterface
+from apps.billing.models import SageX3TransactionInformation, Transaction
+from apps.billing.services.financial_processor_service import TransactionProcessorInterface
 from apps.billing.services.processor_service import SageX3Processor
 
 
@@ -11,13 +11,35 @@ class TransactionService:
     a transaction to the transaction processor.
     """
 
-    def __init__(self) -> None:
-        self.__processor: TransactionProcessorInterface = ProcessorInstantiator(processor=SageX3Processor)
+    def __init__(
+        self,
+        transaction: Transaction,
+    ) -> None:
+        self.transaction = transaction
+        self.__processor: TransactionProcessorInterface = SageX3Processor()
+        self.__processor.transaction = transaction
 
-    def __check_transaction_state(self, document_id):
-        pass
+    def __save_transaction_xml(self, transaction: Transaction, informations: dict) -> None:
+        """
+        Saves the XML content of a transaction to the database.
 
-    def send_transaction_to_processor(self, transaction: Transaction) -> str:
+        This function attempts to get or create a SageX3TransactionInformation object
+        with the given transaction. If the SageX3TransactionInformation object doesn't exist,
+        it creates one with the provided XML content. If an exception occurs during
+        this process, it prints the exception message.
+        """
+        try:
+            created, obj = SageX3TransactionInformation.objects.get_or_create(
+                transaction=transaction, defaults={**informations}
+            )
+            if not created:
+                obj.retries += 1
+                obj.save()
+
+        except Exception as e:
+            print(str(e))
+
+    def send_transaction_to_processor(self) -> str:
         """
         This method receives a Transaction to send to the processor and deals with the request result.
 
@@ -26,18 +48,18 @@ class TransactionService:
         """
 
         try:
-            response_from_service = self.__processor.send_transaction_to_processor(transaction=transaction)
-            response_from_service = response_from_service["soapenv:Envelope"]["soapenv:Body"]
+            response_from_service = self.__processor.send_transaction_to_processor()
+            response_as_json = dict(xmltodict.parse(response_from_service))
+            response_as_json = response_as_json["soapenv:Envelope"]["soapenv:Body"]
 
-            if "multiRef" in list(dict(response_from_service).keys()):
+            if "multiRef" in list(dict(response_as_json).keys()):
                 message = "Nº Fatura NAU já registada no documento: "
-                if message in response_from_service["multiRef"]["message"]:
-                    document_id = response_from_service["multiRef"]["message"].replace(message, "")
-                    self.__check_transaction_state(document_id=document_id)
+                if message in response_as_json["multiRef"]["message"]:
+                    document_id = response_as_json["multiRef"]["message"].replace(message, "")
 
                     return document_id
 
-            result = xmltodict.parse(response_from_service["wss:saveResponse"]["saveReturn"]["resultXml"]["#text"])
+            result = xmltodict.parse(response_as_json["wss:saveResponse"]["saveReturn"]["resultXml"]["#text"])
             document_id = ""
             for r in result["RESULT"]["GRP"]:
                 for field in r["FLD"]:
@@ -50,11 +72,19 @@ class TransactionService:
 
             return document_id
         except Exception as e:
-            print(str(e))
-
-    def run_steps_to_send_transaction(self, transaction: Transaction) -> None:
-        try:
-            document_id = self.send_transaction_to_processor(transaction=transaction)
-            self.__check_transaction_state(document_id=document_id)
-        except Exception as e:
             raise e
+
+    def run_steps_to_send_transaction(self) -> None:
+        try:
+            document_id = self.send_transaction_to_processor()
+            self.__save_transaction_xml(
+                informations={"input_xml": self.__processor.data, "error_messages": ""},
+                transaction=self.transaction,
+            )
+
+            return document_id
+        except Exception as e:
+            self.__save_transaction_xml(
+                informations={"input_xml": self.__processor.data, "error_messages": str(e)},
+                transaction=self.transaction,
+            )
