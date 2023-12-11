2e028875e5a7385557131a85d7d13f8a98f20b7f
jamo.qs <jamo.qs@gmail.com>
2023-12-11 16:08:20 +0000

    chore: celery dynamics

--- nau_financial_manager/celery.py
+++ nau_financial_manager/celery.py
@@ -3,8 +3,10 @@ import os
 from celery import Celery
 from django.conf import settings
 
+from nau_financial_manager.settings import CELERY_BROKER_URL
+
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nau_financial_manager.settings")
 
-app = Celery("nau_financial_manager", backend="redis", broker="redis://nau-redis:6379")
+app = Celery("nau_financial_manager", backend="redis", broker=CELERY_BROKER_URL)
 app.config_from_object("django.conf:settings", namespace="CELERY")
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
