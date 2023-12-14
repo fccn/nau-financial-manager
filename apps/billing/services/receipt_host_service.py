2ce5a264fc94efc2c3cbc6ba6a9433d1c8d96a9f
jamo.qs <jamo.qs@gmail.com>
2023-12-14 17:42:12 +0000

    fix: ajusted the authorization for ilink connection

--- apps/billing/services/receipt_host_service.py
+++ apps/billing/services/receipt_host_service.py
@@ -7,24 +7,26 @@ from django.conf import settings
 class ReceiptDocumentHost:
     def __init__(self) -> None:
         self.__receipt_host_url = getattr(settings, "RECEIPT_HOST_URL")
-        self.__receipt_bearer_token = getattr(settings, "RECEIPT_BEARER_TOKEN")
+        self.__receipt_host_auth = getattr(settings, "RECEIPT_HOST_AUTH")
+        self.__receipt_host_password = getattr(settings, "RECEIPT_HOST_PASSWORD")
         self.__receipt_entity_public_key = getattr(settings, "RECEIPT_ENTITY_PUBLIC_KEY")
-        
+
     def get_document(self, document_id: str):
         """
         This method gets the file url, it calls the receipt host giving the required parameters.
 
-        The `document_id` is a transaction parameter, it is the returned id from the transaction processor.
-        The `__receipt_bearer_token` is setted in the environment.
+        document_id comes from the transaction. It is the returned id from the transaction processor.
+        __receipt_host_auth and  __receipt_host_password is setted in the environment.
         """
 
         try:
             response = requests.get(
                 url=f"{self.__receipt_host_url}/{document_id}",
-                headers={
-                    "entity": self.__receipt_entity_public_key,
-                    "Authorization": f"Bearer {self.__receipt_bearer_token}",
-                },
+                headers={"entity": self.__receipt_entity_public_key},
+                auth=(
+                    self.__receipt_host_auth,
+                    self.__receipt_host_password,
+                ),
             ).content
             response = json.JSONDecoder().decode(response)
             document_informations = [
