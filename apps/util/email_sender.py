ed05682ee413436e4c9b5e8f8e17d7310db73103
Tiago-da-Silva23 <tsilva@quanticosolutions.com>
2023-12-20 14:21:11 +0000

    feat: added first architecture proposal to send email

--- apps/util/email_sender.py
+++ apps/util/email_sender.py
@@ -1,7 +1,14 @@
+import logging
+from datetime import datetime
+
+
 class EmailSender:
     @staticmethod
     def send_email(sender: str, receiver: str, content: str):
         try:
-            ...
+            # TODO: insert the send email business logic
+            logging.getLogger("nau_financial_manager").info(
+                f"info: email sent, time: {datetime.now()}, sender: {sender}, receiver: {receiver}, content: {content}"
+            )
         except Exception as e:
             raise e
