90726009efd936f2c319400572862c363224f633
Ivo Branco <ivo.branco@fccn.pt>
2024-03-11 16:28:03 +0000

    fix: no logging on django command
    fixes #274

--- nau_financial_manager/settings.py
+++ nau_financial_manager/settings.py
@@ -223,7 +223,19 @@ AUTH_PASSWORD_VALIDATORS = [
 
 _LOGGING = CONFIG.get(
     "LOGGING",
-    None,
+    {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "handlers": {
+            "console": {
+                "class": "logging.StreamHandler",
+            },
+        },
+        "root": {
+            "handlers": ["console"],
+            "level": "INFO",
+        },
+    },
 )
 if _LOGGING:
     LOGGING = _LOGGING
